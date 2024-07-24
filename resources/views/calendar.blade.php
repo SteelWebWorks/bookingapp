cbdba222b6f5a919ebdc2272d0536ed3b2863dc0
Zsigmond G. Balázs <steelwebworks@gmail.com>
2024-07-24 13:22:11 +0200

    Tailwind and base views

---
+++ resources/views/calendar.blade.php
@@ -0,0 +1,7 @@
+@extends('container')
+
+@section('content')
+    <div class="pt-2">
+        <span>Fullcanlendar</span>
+    </div>
+@endsection
---
+++ resources/views/container.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('body')
+    <div class="container mx-auto">
+        @yield('content')
+    </div>
+@endsection
